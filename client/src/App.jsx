a6ce1829a80c0e60034ef3ffc854f4d3f7631172
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-06-15 13:19:48 +0600

    Editor Style Added
    Quill Toolbar is added and styled

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import TextEditor from './TextEditor';
 function App() {
   return (
     <>
-      <h1>Google Doc Alt</h1>
       <TextEditor />
     </>
   );
--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -1,16 +1,36 @@
-import { useCallback } from 'react';
+import { useCallback, useRef } from 'react';
 import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
 
+const toolbarOptions = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: 'ordered' }, { list: 'bullet' }],
+  ['bold', 'italic', 'underline'],
+  [{ color: [] }, { background: [] }],
+  [{ script: 'sub' }, { script: 'super' }],
+  [{ align: [] }],
+  ['image', 'blockquote', 'code-block'],
+  ['clean'],
+];
+
 const TextEditor = () => {
+  const quillRef = useRef(null); // store Quill instance
+
   const wrapperRef = useCallback((wrapper) => {
-    if (wrapper == null) return;
+    if (wrapper == null || quillRef.current) return;
 
     wrapper.innerHTML = '';
 
     const editor = document.createElement('div');
     wrapper.append(editor);
-    new Quill(editor, { theme: 'snow' });
+
+    quillRef.current = new Quill(editor, {
+      theme: 'snow',
+      modules: {
+        toolbar: toolbarOptions,
+      },
+    });
   }, []);
 
   return <div className="container" ref={wrapperRef}></div>;
